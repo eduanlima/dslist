d57dca3f870898f9a00248cb459e10b0737a40c3
eduanlima@github.com <eduandev@gmail.com>
2025-05-22 19:07:10 -0300

    Setting properties and add model

---
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platform;
+	private String imgUrl;
+	private String shortDescription;
+	private String longDescription;
+}
